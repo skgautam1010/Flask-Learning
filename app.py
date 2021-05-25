24007488a416283aefbc9acecbb1a9899b8ff3fd
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-05-25 23:33:33 +0530

    adding data to the database through flask

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Skg@123!@localhost/flask_learning'
+db=SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    fname=db.Column(db.String(50),unique=False,nullable=False)
+    lname = db.Column( db.String( 50 ), unique=False, nullable=False )
+    contact = db.Column( db.String( 12 ), unique=True, nullable=False )
+    email = db.Column( db.String( 255 ), unique=True, nullable=False )
+    msg = db.Column( db.String( 100 ), unique=False, nullable=False )
 
 @app.route('/index.html')
 def index():
@@ -9,11 +22,19 @@ def index():
 def aboutus():
     return render_template('aboutus.html')
 
-@app.route("/bootstrap")
-def bootstrap():
-    return render_template('bootstrap.html')
 
-@app.route("/contactus.html")
+@app.route("/contactus.html",methods=['GET','POST'])
 def contact():
+    if(request.method=='POST'):
+        f_name=request.form.get('firstname')
+        l_name=request.form.get('lastname')
+        contactus=request.form.get('telephone')
+        emailid=request.form.get('emailid')
+        feedback=request.form.get('feedback')
+
+        entry=Contact(fname=f_name,lname=l_name,contact=contactus,email=emailid,msg=feedback)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contactus.html')
+
 app.run(debug=True)
